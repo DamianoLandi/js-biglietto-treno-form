19b78ce3982abf7d1436c203c6e2ee7f6161637a
DamianoLandi <damianolandi01@gmail.com>
2021-07-08 17:10:21 +0200

    Raccolta Variabili

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 console.log("ok");
+
+//Raccolta Variabili
+
+var nameInput = document.getElementById("name");
+
+var kmsInput = document.getElementById("kms");
+
+var ageInput = document.getElementById("age");
+
+var generate = document.getElementById("generate");
+
+var reset = document.getElementById("reset");
+
+var nameDisplay = document.getElementById("name-display");
+
+var ageDisplay = document.getElementById("age-display");
+
+var discountDisplay = document.getElementById("discount");
+
+var carDisplay = document.getElementById("car-display");
+
+var codeDisplay = document.getElementById("code-display");
